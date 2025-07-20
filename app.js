b4e99874c192f40b48c0e8f15d3a2086371d86a8
AmruthaBinu <amruthabinu133@gmail.com>
2025-07-20 14:08:47 +0530

    signup complete

--- app.js
+++ app.js
@@ -3,13 +3,49 @@ const Mongoose = require("mongoose")
 const Cors = require("cors")
 const Bcrypt = require("bcrypt")
 const Jwt = require("jsonwebtoken")
+const userModel = require("./models/users")
 
-let app=Express()
+let app = Express()
 
-app.get("/",(req,res)=>{
-    res.json("hello")
+
+app.use(Express.json())
+app.use(Cors())
+
+Mongoose.connect("mongodb+srv://amruthabinu:amruthabinu2002@cluster0.bwn2sfy.mongodb.net/blogDb?retryWrites=true&w=majority&appName=Cluster0")
+
+app.post("/signup", async (req, res) => {
+
+    let input = req.body
+    let hashedPassword = Bcrypt.hashSync(req.body.password, 10)
+    console.log(hashedPassword)
+    req.body.password = hashedPassword
+
+
+    userModel.find({ email: req.body.email }).then(
+        (items) => {
+            if (items.length > 0) 
+            {
+                res.json({ "status": "email id already exist" })
+            }
+            else 
+            {
+                let result = new userModel(input)
+                result.save()
+                res.json({ "status": "success" })
+            }
+
+        }
+    ).catch(
+        (error) => { }
+    )
+
+
+
+
 })
 
-app.listen(3030,()=>{
+
+
+app.listen(3030, () => {
     console.log("server is running")
 })
