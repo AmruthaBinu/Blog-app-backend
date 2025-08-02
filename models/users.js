6656afc5a534e2aceb1cec59e337cd30b920af33
AmruthaBinu <amruthabinu133@gmail.com>
2025-08-02 11:52:16 +0530

    signin complete

--- models/users.js
+++ models/users.js
@@ -10,6 +10,10 @@ const userSchema = Mongoose.Schema(
         email: {
             type:String,
             required:true
+        },
+        password: {
+            type:String,
+            required:true
         }
     }
 )
