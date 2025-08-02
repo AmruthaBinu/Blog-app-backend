f59d4b79feb0bb5577f2179075903d78fc8b9e56
AmruthaBinu <amruthabinu133@gmail.com>
2025-08-02 21:50:41 +0530

    viewall complete

--- app.js
+++ app.js
@@ -36,14 +36,39 @@ app.post("/create",async(req,res)=>{
         }
     })
 
-
-
-
-
-
-
-
 })
+
+// Viewall posts
+app.post("/viewall",(req,res)=>{
+    let token= req.headers.token
+
+    Jwt.verify(token,"blogApp",(error,decoded)=>{
+        if (decoded && decoded.email){
+
+            postModel.find().then(
+                (items)=>{
+                    res.json(items)
+                }
+            ).catch(
+                (error)=>{
+                    res.json({"status":"error"})
+                }
+            )
+
+        }else{
+
+            res.json({"status":"Invalid authentication"})
+        }
+    })
+})
+
+
+
+
+
+
+
+
 
 // SignIn
 app.post("/signin", async (req, res) => {
